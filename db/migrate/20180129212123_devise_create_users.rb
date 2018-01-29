449fd23ae5dc4a77a146653d3cb70584ca83f650
John Maddux <jemaddux@gmail.com>
2018-01-29 16:46:05 -0500

    First page basics

--- db/migrate/20180129212123_devise_create_users.rb
+++ db/migrate/20180129212123_devise_create_users.rb
@@ -3,8 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :first_name, null: false
-      t.string :last_name,  null: false
+      t.string :first_name
+      t.string :last_name
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
